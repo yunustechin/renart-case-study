9f975c00c29e971d8c77222edba685bbdda0bf38
yunustechin <yunustechin@gmail.com>
2025-07-05 16:11:36 +0300

    utils

---
+++ utils/logger.js
@@ -0,0 +1,17 @@
+const winston = require('winston');
+
+const consoleFormat = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`
+  )
+);
+
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+  format: consoleFormat,
+  transports: [new winston.transports.Console()],
+});
+
+module.exports = logger;
