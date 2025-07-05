11ebb01fafc973db239c313c7dc0c7d5b5391aac
yunustechin <yunustechin@gmail.com>
2025-07-05 16:07:37 +0300

    app

---
+++ api/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const productRoutes = require('./products');
+const errorHandler = require('../utils/errorHandler');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/products', productRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  error.statusCode = 404;
+  next(error);
+});
+
+app.use(errorHandler);
+
+module.exports = app;
