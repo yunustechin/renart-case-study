7de89837395440e576055bf31f184ebae6e93503
yunustechin <yunustechin@gmail.com>
2025-07-06 00:44:20 +0300

    app

---
+++ ui/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductList from './components/ProductList/ProductList';
+import './App.css'; 
+
+function App() {
+  return (
+    <main className="product-showcase-app">
+      <ProductList />
+    </main>
+  );
+}
+
+export default App;
