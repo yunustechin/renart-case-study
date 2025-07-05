d03d3e123daaf0ff3cc2bc33f37d5704330f1db1
yunustechin <yunustechin@gmail.com>
2025-07-05 17:05:25 +0300

    app

--- api/app.js
+++ api/app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const productRoutes = require('./products');
 const errorHandler = require('../utils/errorHandler');
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/products', productRoutes);
 
 app.use((req, res, next) => {
