d354d36d23bb2f071174751a97950d0a3009ac4e
yunustechin <yunustechin@gmail.com>
2025-07-05 17:06:48 +0300

    products

--- api/products.js
+++ api/products.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { getAllProducts, getProductById } = require('./productController');
-
+const { validateProductId, handleValidationErrors } = require('./productValidator');
 const router = express.Router();
 
 router.get('/', getAllProducts);
-
-router.get('/:id', getProductById);
+router.get(
+  '/:id',
+  validateProductId,     
+  handleValidationErrors, 
+  getProductById          
+);
 
 module.exports = router;
