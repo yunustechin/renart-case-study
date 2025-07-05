0c2e42999991a02451559c73ac95bd2bb0da3ca9
yunustechin <yunustechin@gmail.com>
2025-07-05 16:08:22 +0300

    products

---
+++ api/products.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { getAllProducts, getProductById } = require('./productController');
+
+const router = express.Router();
+
+router.get('/', getAllProducts);
+
+router.get('/:id', getProductById);
+
+module.exports = router;
