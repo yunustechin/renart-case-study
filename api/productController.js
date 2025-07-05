ef080b98a2e0e902722d3af8dea3b0c61453d7ea
yunustechin <yunustechin@gmail.com>
2025-07-06 00:33:21 +0300

    product controller

--- api/productController.js
+++ api/productController.js
@@ -1,14 +1,21 @@
-const productService = require('./productService');
+import * as productService from './productService.js';
 
 /**
  * Express controller to handle the request for getting all products.
+ * It now supports filtering by price range and popularity score.
  * @param {object} req - Express request object.
  * @param {object} res - Express response object.
  * @param {function} next - Express next middleware function.
  */
-const getAllProducts = async (req, res, next) => {
+export const getAllProducts = async (req, res, next) => {
   try {
-    const products = await productService.getProducts();
+    const { minPrice, maxPrice, popularityScore } = req.query;
+    const filters = {
+      minPrice: minPrice ? parseFloat(minPrice) : undefined,
+      maxPrice: maxPrice ? parseFloat(maxPrice) : undefined,
+      popularityScore: popularityScore ? parseFloat(popularityScore) : undefined,
+    };
+    const products = await productService.getProducts(filters);
     res.status(200).json(products);
   } catch (error) {
     next(error);
@@ -21,7 +28,7 @@ const getAllProducts = async (req, res, next) => {
  * @param {object} res - Express response object.
  * @param {function} next - Express next middleware function.
  */
-const getProductById = async (req, res, next) => {
+export const getProductById = async (req, res, next) => {
   try {
     const { id } = req.params;
     const product = await productService.getProductById(id);
@@ -38,7 +45,3 @@ const getProductById = async (req, res, next) => {
   }
 };
 
-module.exports = {
-  getAllProducts,
-  getProductById,
-};
