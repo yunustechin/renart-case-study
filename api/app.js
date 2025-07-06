8155e3a43c6caba52aadcf4e2c5964e85f880288
yunustechin <yunustechin@gmail.com>
2025-07-06 11:49:01 +0300

    app

--- api/app.js
+++ api/app.js
@@ -2,10 +2,24 @@ import express from 'express';
 import cors from 'cors';
 import productRoutes from './products.js'; 
 import errorHandler from '../utils/errorHandler.js'; 
+import logger from '../utils/logger.js';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    logger.info(
+      `${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`
+    );
+  });
+  next();
+});
+
 app.use('/products', productRoutes);
 
 app.use((req, res, next) => {
